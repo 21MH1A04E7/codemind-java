74eae2be7c9f01d84eb6e97fede1b9af19b2dd89
21MH1A04E7 <97781788+21MH1A04E7@users.noreply.github.com>
2023-01-15 21:51:02 +0530

    import java.util.Scanner; class Hariom { 	public static void main(String[] args) { 	    float a,b;         Scanner sc=new Scanner(System.in);         a=sc.nextFloat();         b=sc.nextFloat();         System.out.format("%.2f", a*b); 	} }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Hariom
+{
+	public static void main(String[] args) {
+	    float a,b;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextFloat();
+        b=sc.nextFloat();
+        System.out.format("%.2f", a*b);
+	}
+}
